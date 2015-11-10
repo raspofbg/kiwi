8821cfaf10e1ee40d3870b38b90dbcc60458b2c7
Bruno <bruno.gasc@gmail.com>
2015-11-10 11:29:27 +0100

    Create __init__.py

---
+++ dom/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python -O 
+# -*- coding: utf-8 -*-
+# Projet : kiwi
+# Fichier : /data/web/kiwi/dom/__init.py__
+# Date de création : 7 octobre 2015 (bg)
+# Date de modification : 7 octobre 2015 (bg)
